0c8765d25fdc4f54bd4ee78c2323151595e2da3c
ishtiyak98 <rifatishtiyak@gmail.com>
2023-12-14 05:00:26 +0600

    project with sass config

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p>This is home</p>
+  <h1 class="text-h1" style="font-size: 170px">ok</h1>
+  <v-btn elevation="16" size="x-large">KKKk 16</v-btn>
+</template>
